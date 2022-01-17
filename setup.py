c75a66dc97169c50e227cb1b7fea8db9f4388003
7sDream <i@7sdre.am>
2022-01-17 15:50:21 +0800

    fix: setup.py classifiers

--- setup.py
+++ setup.py
@@ -9,37 +9,35 @@ except ImportError:
 import torrent_parser
 
 setup(
-    name='torrent_parser',
-    keywords=['file', 'torrent', 'JSON', 'parser'],
+    name="torrent_parser",
+    keywords=["file", "torrent", "JSON", "parser"],
     version=torrent_parser.__version__,
-    py_modules=['torrent_parser'],
-    url='https://github.com/7sDream/torrent_parser',
-    license='MIT',
-    author='7sDream',
-    author_email='i@7sdre.am',
-    description='A .torrent file parser and creator for both Python 2 and 3',
+    py_modules=["torrent_parser"],
+    url="https://github.com/7sDream/torrent_parser",
+    license="MIT",
+    author="7sDream",
+    author_email="i@7sdre.am",
+    description="A .torrent file parser and creator for both Python 2 and 3",
     install_requires=[],
-    entry_points={
-        'console_scripts': ['pytp=torrent_parser:__main']
-    },
+    entry_points={"console_scripts": ["pytp=torrent_parser:__main"]},
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
-        'Programming Language :: Python :: 3.8'
-        'Programming Language :: Python :: 3.9'
-        'Programming Language :: Python :: 3.10'
-        'Topic :: Multimedia',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Utilities',
-    ]
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Multimedia",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
 )
